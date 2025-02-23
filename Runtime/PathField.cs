b5caad8aa4fc444001b0700e18ff5fb9f6270207
Vladislav Suvorov <rabsuira@proton.me>
2025-02-23 22:15:04 +0300

    Added convertation to string

--- Runtime/PathField.cs
+++ Runtime/PathField.cs
@@ -4,5 +4,16 @@ namespace InspectorPathField
     public class PathField
     {
         public string AssetPath;
+
+        public override string ToString()
+        {
+            return AssetPath;
+        }
+
+
+	public static implicit operator string(PathField pathField)
+    	{
+        	return pathField?.AssetPath;
+    	}
     }
 }
