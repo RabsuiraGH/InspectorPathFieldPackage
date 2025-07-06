11bda6270107f2608fcef96f3a03c0333ef09314
RabsuiraGH <rabsuira@proton.me>
2025-07-06 17:44:31 +0300

    Added Attribute support

---
+++ Editor/PathFieldAttributeDrawer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace InspectorPathField.Editor
+{
+    [CustomPropertyDrawer(typeof(PathFieldAttribute))]
+    internal class PathFieldAttributeDrawer : PathFieldBaseDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            if (property.propertyType != SerializedPropertyType.String)
+            {
+                EditorGUI.LabelField(position, label.text, "Use [PathField] with string only!");
+                return;
+            }
+            base.OnGUI(position, property, label);
+        }
+    }
+}
---
+++ Editor/PathFieldClassDrawer.cs
@@ -0,0 +1,14 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace InspectorPathField.Editor
+{
+    [CustomPropertyDrawer(typeof(PathField))]
+    internal class PathFieldClassDrawer : PathFieldBaseDrawer
+    {
+        public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+        {
+            base.OnGUI(position, property.FindPropertyRelative("AssetPath"), label);
+        }
+    }
+}
